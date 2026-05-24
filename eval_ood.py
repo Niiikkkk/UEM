97a3c7fcd575ff8f99cbdc4851df2c3bbcfcaeff
nicholas.berardo <berardonicholas5@gmail.com>
2026-05-24 21:24:51 +0200

    tmp

--- eval_ood.py
+++ eval_ood.py
@@ -118,7 +118,7 @@ def main(args):
                 run_path = os.path.join(w_path,run)
                 images = glob.glob(f"{run_path}/rgb/*.png")
                 #Check those images and get some new
-                img = images[100]
+                img = images[70]
                 #images = images[50:]
                 #len_final = min(len(images)-1,30)
                 #images = images[:len_final]
